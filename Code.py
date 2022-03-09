f1f5cc64678fd00814725348f489bb857246bd6d
FrenchCamp <97358887+FrenchCamp@users.noreply.github.com>
2022-03-09 08:11:18 +0100

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,14 @@
+#Script by FrechCamp
+
+def main():
+ print("[1] - Print")
+print("[99] - Quit")
+
+def script():
+  main()
+  ins = input("Entrer")
+  
+  while ins != 99:
+    
+    if ins == 1:
+      print("Option 1")
